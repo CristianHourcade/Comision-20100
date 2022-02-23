cc54c7d255dd0d553640ce988a9c0b122546ea6b
Cristian Hourcade <hourcadecristian@gmail.com>
2022-02-23 20:51:42 -0300

    Clase de bucle

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const tiempoDeEntregaDeRapi = 45;
+
+switch(tiempoDeEntregaDeRapi) {
+    case 10: 
+        console.log("Llego a los 10 minutos (/-.-)/");
+        break;
+    case 32: 
+        console.log("Llegaste a los 32 minutos, un toque tarde");
+        break;
+    case 45: 
+        console.log("45 minutos o gratis");
+        break;
+    default: 
+        console.log("no sabemos en que estado esta tu pedido");
+        break;
+}
