cacac01083c1d55671ebf90a46368557940c8d1e
Cristian Hourcade <hourcadecristian@gmail.com>
2022-02-23 20:58:22 -0300

    Ejercicio

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
-const tiempoDeEntregaDeRapi = 45;
+/** Mostrar en consola si el alumno esta aprobado o no */
+const proyectosFinales = [10, 6, 3, 6, 8, 2];
 
-switch(tiempoDeEntregaDeRapi) {
-    case 10: 
-        console.log("Llego a los 10 minutos (/-.-)/");
-        break;
-    case 32: 
-        console.log("Llegaste a los 32 minutos, un toque tarde");
-        break;
-    case 45: 
-        console.log("45 minutos o gratis");
-        break;
-    default: 
-        console.log("no sabemos en que estado esta tu pedido");
-        break;
+for (...){
+    ...
 }
