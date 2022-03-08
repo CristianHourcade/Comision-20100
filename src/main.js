85b054255fc1333e9f8e9d0cd2eb56c53453748d
Cristian Hourcade <hourcadecristian@gmail.com>
2022-03-07 21:00:05 -0300

    Cards generadas, CLASE ARRAY

--- src/main.js
+++ src/main.js
@@ -1,32 +1,65 @@
-const PRODUCTO = new Producto('Zapas niky', 900, 'zapatillas');
+// Forma 1
+// function agregarAlCarrito(producto) {
+//     carrito.push(producto);
+// }
 
-PRODUCTO.agregarAlCarrito('Totito');
-PRODUCTO.titulo;
-PRODUCTO.categoria = "aloha";
+// Forma 2
+const agregarAlCarrito = (producto) => {
+    carrito.push(producto);
+};
 
-function Producto(titulo, precio, categoria){
-    this.titulo = titulo;
-    this.precio = precio;
-    this.categoria = categoria;
-    this.agregarAlCarrito = function(nombreDelCliente) { 
-        console.log("me agregaste al carrito" + nombreDelCliente);
-    }
-}
+const carrito = [];
+const productos = [
+    { id: 1, titulo: "Zapa niky", precio: 999 },
+    { id: 2, titulo: "Remera Puma", precio: 222 },
+    { id: 3, titulo: "Pantalon Adidas", precio: 111 },
+    { id: 4, titulo: "Airmax", precio: 1982 },
+    { id: 5, titulo: "Jagguar", precio: 321 },
+    { id: 6, titulo: "Chancla", precio: 733 },
+]
 
-// class Producto {
-//     static titulo = "Totito";
 
-//     constructor(titulo, precio, categoria){
-//         this.titulo = titulo;
-//         this.precio = precio;
-//         this.categoria = categoria;
-//     };
 
-//     agregarAlCarrito(){
+let cardsGeneradas = ``;
 
-//     }
-// };
+productos.forEach(elementoDelArray => {
+    cardsGeneradas += `<div class="card">
+        <div class="card-header">
+            ${elementoDelArray.titulo}
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">$ ${elementoDelArray.precio}</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary" onclick="agregarAlCarrito()">Go somewhere</a>
+        </div>
+    </div>`;
+})
 
-for(let i = 0; i<10; i++){
-    console.log(i);
-}
+document.write(cardsGeneradas);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const arrayProductos = ["Zapa1", "Remera1", "Pantalon3"];
+
+
+const indiceDelProducto = arrayProductos.indexOf("Remera1");
+// console.log(indiceDelProducto);
+
+
+arrayProductos.splice(indiceDelProducto, 1);
+// console.log(arrayProductos);
+
+
+// SPLICE -> BORRA
+// SLICE -> COPIA
+// SPLIT -> CREA UN ARRAY SEGUN UN CARACTER
