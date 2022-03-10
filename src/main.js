f39a26572523adff30a1ac7c968d3737bac7ac90
Cristian Hourcade <hourcadecristian@gmail.com>
2022-03-09 21:00:06 -0300

    Clase 7

--- src/main.js
+++ src/main.js
@@ -11,23 +11,61 @@ const agregarAlCarrito = (producto) => {
 const carrito = [];
 
 const productos = [
-    { id: 1, titulo: "Zapa niky", precio: 999 },
-    { id: 2, titulo: "Remera Puma", precio: 222 },
-    { id: 3, titulo: "Pantalon Adidas", precio: 111 },
-    { id: 4, titulo: "Airmax", precio: 1982 },
-    { id: 5, titulo: "Jagguar", precio: 321 },
-    { id: 6, titulo: "Chancla", precio: 733 },
+    { id: 1, titulo: "Zapa niky", precio: 999, stock:0 },
+    { id: 2, titulo: "Remera Puma", precio: 222, stock:0 },
+    { id: 3, titulo: "Pantalon Adidas", precio: 111, stock:0},
+    { id: 4, titulo: "Airmax", precio: 1982, stock:56 },
+    { id: 5, titulo: "Jagguar", precio: 321, stock:0 },
+    { id: 6, titulo: "Chancla", precio: 733, stock:12 },
 ];
 
-const productosEnOferta = [
-    { id: 1, titulo: "Zapa niky", precio: 1 },
-    { id: 2, titulo: "Remera Puma", precio: 1 },
-    { id: 3, titulo: "Pantalon Adidas", precio: 1 },
-];
+// Sumar total del carrito
+const totalDelCarrito = productos.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+console.log("Total del carrito:" + totalDelCarrito);
+
+// Crear un array apartir de otro
+const nuevoListadoProductos = productos.map((producto) => {
+    if(producto.stock === 0){
+        producto.stock = "No hay stock";
+    }
+    return producto;
+})
+console.log(nuevoListadoProductos);
+
+// Codigo para filtrar por precio/caegoria/etc
+const productosDeMayorPrecio = productos.filter((producto) => producto.precio > 750 );
+console.log(productosDeMayorPrecio);
+generarCards(productosDeMayorPrecio);
+
+
+// Codigo para buscador
+const valorDelBucador = "remera Puma"
+const productoEncontrado = productos.find(
+    (producto) => producto.titulo.toUpperCase().trim() === valorDelBucador.toUpperCase().trim());
+// console.log(productoEncontrado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function generarCards(productosAMostrar){
     let acumuladorDeCards = ``;
-    productosAMostrar.forEach(elementoDelArray => {
+    productosAMostrar.forEach((elementoDelArray) => {
         acumuladorDeCards += `<div class="card">
             <div class="card-header">
                 ${elementoDelArray.titulo}
@@ -39,11 +77,17 @@ function generarCards(productosAMostrar){
             </div>
         </div>`;
     });
-    return acumuladorDeCards;
+    mostrarCardsEnElHTML(acumuladorDeCards);
+
 }
 
-const cardsGeneradas = generarCards(productosEnOferta);
-document.write(cardsGeneradas);
+generarCards(productosEnOferta);
+
+function mostrarCardsEnElHTML(cards) {
+    document.write(cards);
+};
+// const mostrarCardsEnElHTML = (cards) => document.write(cards);
+
 
 
 
