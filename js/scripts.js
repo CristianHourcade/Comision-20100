8b63ce50d469c4e041c7598fb6f24897691d463c
Cristian Hourcade <hourcadecristian@gmail.com>
2022-04-05 19:34:14 -0300

    todos los m,etodos principales

--- js/scripts.js
+++ js/scripts.js
@@ -1,154 +1,72 @@
-// const storageCarrito = JSON.parse(localStorage.getItem("carrito"));
 
-// const subtotalCart = cart.reduce((acumulador, next) => acumulador + next.precio, 0);
-// console.log(`El subtotal de tu carrito es: ${subtotalCart}`);
-
-const productos = [
-    { id: 1, titulo: "Zapa niky", precio: 999, stock: 0, imagen: 'https://estaticos-cdn.prensaiberica.es/clip/a5ee7a2a-6f63-4ab9-8986-ba83113aca56_16-9-discover-aspect-ratio_default_0.jpg' },
-    { id: 2, titulo: "Remera Puma", precio: 222, stock: 32, imagen: 'https://estaticos-cdn.prensaiberica.es/clip/a5ee7a2a-6f63-4ab9-8986-ba83113aca56_16-9-discover-aspect-ratio_default_0.jpg' },
-    { id: 3, titulo: "Pantalon Adidas", precio: 111, stock: 0, imagen: 'https://estaticos-cdn.prensaiberica.es/clip/a5ee7a2a-6f63-4ab9-8986-ba83113aca56_16-9-discover-aspect-ratio_default_0.jpg' },
-    { id: 4, titulo: "Airmax", precio: 1982, stock: 56 },
+const listaProductos = [
+    { id: 1,
+      nombre: "Pachata", 
+      precio: 600, 
+      stock: 10,
+      imagen: "https://images.deliveryhero.io/image/pedidosya/products/16480133-07c02758-51e5-48bc-848a-958de32372f0.jpeg?quality=90&width=248"
+    },
+    { id: 2, 
+      nombre: "Lomo", 
+      precio:800, 
+      stock: 10,
+      imagen: "https://www.circuitogastronomico.com/wp-content/uploads/2020/08/pizzar-lomo.jpg"
+    },
+    { id: 3, 
+      nombre: "Hamburguesa", 
+      precio: 400, 
+      stock: 10,
+      imagen: "https://okdiario.com/img/2021/05/28/hamburguesa-3-655x368.jpg"
+    },
+    { id: 4, 
+      nombre: "Papas fritas", 
+      precio: 250, 
+      stock: 10,
+      imagen: "http://c.files.bbci.co.uk/104B4/production/_103104766_gettyimages-957724442.jpg"
+    },
+    { id: 5, 
+      nombre: "Pizza Peperoni", 
+      precio: 950, 
+      stock: 10,
+      imagen: "https://www.saborusa.com/wp-content/uploads/2019/10/Animate-a-disfrutar-una-deliciosa-pizza-de-salchicha-Foto-destacada.png"
+    },
+    { id: 6, 
+      nombre: "Barroluco", 
+      precio: 1000, 
+      stock: 10,
+      imagen: "https://www.diariodecuyo.com.ar/export/sites/diariodecuyo/img/2021/02/05/barros_1.jpg"
+    }
+  
 ];
 
 
+const agregarAlCarrito = (idProducto, cantidadAgregados = 1) =>{
 
-
-
-const agregarAlCarrito = (idProducto) => {
-    const valorDeCantidad = document.getElementById(
-        `cantidad-${idProducto}`
-    ).value;
-    // Buscando el producto a agregar
-    const productoAgregado = productos.find(producto => producto.id === idProducto);
-    productoAgregado.cantidad = valorDeCantidad;
-
-    // Agregando al carrito
-    carrito.push(productoAgregado);
-
-    // Actualizando el storage del carrito
+    const indiceEncontradoProducto = listaProductos.findIndex((elemento) => {
+        return elemento.id === idProducto;
+    });
+    const productoAgregado = listaProductos[indiceEncontradoProducto]
+    
+    const existeElProducto = carrito.findIndex((producto) => producto.id === productoAgregado.id);
+    
+    existeElProducto ? carrito[existeElProducto].cantidad += cantidadAgregados : carrito.push(productoAgregado);
+    
     localStorage.setItem("carrito", JSON.stringify(carrito));
+    calcularTotalCarrito();
+}
 
-    // Actualizando el html
-    document.getElementById("cantidad-prod").innerHTML = carrito.length;
+const calcularTotalCarrito = () => {
+    const totalCarrito = carrito.reduce((accum, element) => accum + (element.precio * element.cantidad), 0);
 
-    // Actualizar stock
-    // Volver a generar las cards
-};
+    document.getElementById("cart-total").innerHTML = totalCarrito;
+}
 
-const irAlProducto = (idProducto) => {
-    // Buscamos el producto
-    const productoQueQuiereVer = productos.find(producto => producto.id === idProducto);
 
-    localStorage.setItem("productoAVer", JSON.stringify(productoQueQuiereVer));
-    location.href="detalle.html";
-};
-
-generarCards(productos);
-
-function generarCards(productosAGenerar) {
-    let acumuladorDeCards = ``;
-    productosAGenerar.forEach((elementoDelArray) => {
-        acumuladorDeCards += `<div class="col mb-5">
-        <div class="card h-100">
-            <!-- Sale badge-->
-            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">
-                ${(elementoDelArray.stock > 0) ? 'Esta en venta' : 'Out stock'}
-            </div>
-            <!-- Product image-->
-            <img class="card-img-top" src="${elementoDelArray.imagen}" alt="..." />
-            <!-- Product details-->
-            <div class="card-body p-4">
-                <div class="text-center">
-                    <!-- Product name-->
-                    <h5 class="fw-bolder">${elementoDelArray.titulo}</h5>
-                    <!-- Product price-->
-                    <span class="text-muted text-decoration-line-through">$20.00</span>
-                    <input value="1" min="1" id="cantidad-${elementoDelArray.id}" type="number" placeholder="cantidad">
-                    $${elementoDelArray.precio}
-                </div>
-            </div>
-            <!-- Product actions-->
-            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent" >
-                <div class="text-center">
-                    <button 
-                        onclick="agregarAlCarrito(${elementoDelArray.id})"
-                        class="btn btn-outline-dark mt-auto" href="#">
-                        Add to cart
-                    </button>
-                    <button 
-                        onclick="irAlProducto(${elementoDelArray.id})"
-                        class="btn btn-outline-dark mt-auto" href="#">
-                        Ver producto
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>`;
+// Cart.html
+const sumarCantidad = (idProducto) => {
+    const indiceEncontradoProducto = carrito.findIndex((elemento) => {
+        return elemento.id === idProducto;
     });
-    mostrarCardsEnElHTML(acumuladorDeCards);
+    carrito[indiceEncontradoProducto].cantidad += 1;
+    // re-renderizar el html
 }
-
-function mostrarCardsEnElHTML(cards) {
-    document.getElementById("listado-productos").innerHTML = cards;
-};
-
-
-function buscarProducto() {
-    console.log("Hola!")
-    const nombreProductoBuscado = document.getElementById("producto-buscado").value.toUpperCase().trim();
-
-    const productosEncontrados = productos.filter((producto) => {
-        return producto.titulo.toUpperCase().match(nombreProductoBuscado);
-    });
-
-    generarCards(productosEncontrados);
-}
-
-
-//Forma 2 de escuchar un evento
-const boton = document.getElementById("boton-buscar");
-
-// boton.addEventListener('click', buscarProducto);
-
-// boton.onclick = buscarProducto;
-// $("#boton-buscar").click(() => {})
-
-function tomarValor(event) {
-    const valorDelInput = event.target.value;
-    console.log(event.target.type);
-
-
-    if (event.target.type === "password") {
-        if (valorDelInput.length < 8) {
-            event.target.style.border = "2px solid red";
-        } else {
-            event.target.style.border = "2px solid green";
-        }
-    } else {
-        if (valorDelInput.includes("@")) {
-            event.target.style.border = "2px solid green";
-        } else {
-            event.target.style.border = "2px solid red";
-        }
-    }
-    // const input = document.getElementById("texto-prueba").value;
-    // console.log(input);
-}
-
-
-
-/***
- * 
- * Condicionales 9
- * Funciones 
- */
-
-const stock = 10;
-
-if (stock < 0) {
-    console.log("Tenemos stock");
-} else {
-    console.log("No tenemos stock");
-}
-
-// if (condicion) console.log("Tenemos stock");
